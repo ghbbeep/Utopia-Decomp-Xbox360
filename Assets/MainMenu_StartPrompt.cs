74e7228f43937eb9e628852115917e6c4c7eb4d3
MemeMaster9000 <77814425+ghbbeep@users.noreply.github.com>
2025-08-28 18:13:29 -0400

    Initial upload

---
+++ Assets/MainMenu_StartPrompt.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class MainMenu_StartPrompt : MonoBehaviour {
+    // This will show up like Button’s OnClick() in the inspector
+    [System.Serializable]
+    public class MyEvent : UnityEvent { }
+
+    private Text textToBlink;
+    private bool startPressed = false;
+    public float speed = 5f; // controls blink speed
+
+    public MyEvent onStartPressed;
+
+    void Awake()
+    {
+        textToBlink = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (textToBlink != null)
+        {
+            if (startPressed == false)
+            {
+                float alpha = (Mathf.Sin(Time.time * speed) + 1f) / 2f; // oscillates between 0–1
+                Color c = textToBlink.color;
+                c.a = alpha;
+                textToBlink.color = c;
+            }
+            else
+            {
+                float alpha = 0;
+                Color c = textToBlink.color;
+                c.a = alpha;
+                textToBlink.color = c;
+            }
+        }
+
+
+        if (Input.GetButtonDown("Submit"))
+        {
+            StartPressed();
+        }
+    }
+
+    // Call this from code when you want to "invoke" it
+    public void StartPressed()
+    {
+        startPressed = true;
+        onStartPressed.Invoke();
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# SUtopia - Sonic Utopia ReDecompiled
+
+After 10 months...Sonic Utopia has been ReDecompiled with a newer version of AssetRipper!
+
+# Differences between this Decomp and the Legacy Decomp
+
+Everything is 1:1.
+
+Working shaders.
+
+NO additions at all. - I wanted to keep this as a pure vanilla decompilation so there's no DiscordRPC.
+
+Different Unity version! - The legacy decomp uses 2019.4.17f1 (i think), while this one uses the version Sonic Utopia was made with (5.4.1f1)!
+
+# Ports
+
+* None yet.
+
+# Requirements
+
+* [Github Desktop](https://desktop.github.com/) - USE THIS TO DOWNLOAD/CLONE BECAUSE OF GIT LFS STUFF
+
+* [Unity 5.4.1f1](https://unity.com/releases/editor/whats-new/5.4.1f1) - Unity Editor lol
+
+* [Visual Studio Code](https://code.visualstudio.com) (ONLY IF YOU'RE GOING TO EDIT THE CODE)
+
+# Credits
+
+* [SnesFX/Koi (me!)](https://twitter.com/maybekoi_) - Decompilation
+
+* Sonic Utopia Devs - Making the amazing fangame!
